9a3afffcfa0fb0c580eaff46073d7d44bf2be0f7
Itamar Ostricher <itamarost@gmail.com>
2021-05-23 20:32:00 -0700

    Nit styling

--- src/components/CommonForBoth/TopbarDropdown/NotificationDropdown.js
+++ src/components/CommonForBoth/TopbarDropdown/NotificationDropdown.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types"
 import { Link } from "react-router-dom"
 import { Dropdown, DropdownToggle, DropdownMenu, Row, Col } from "reactstrap"
 import SimpleBar from "simplebar-react"
@@ -12,7 +12,6 @@ import avatar4 from "../../../assets/images/users/avatar-4.jpg"
 import { withTranslation } from "react-i18next"
 
 const NotificationDropdown = props => {
-  // Declare a new state variable, which we'll call "menu"
   const [menu, setMenu] = useState(false)
 
   return (
